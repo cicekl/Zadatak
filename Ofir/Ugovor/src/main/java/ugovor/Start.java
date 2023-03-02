c3a63f97e6f53c4d24099723ca1ad8a67da2b556
Lorena <cicek.lorena@gmail.com>
2023-03-02 13:04:08 +0100

    Nova datoteka stvorena

--- Ofir/Ugovor/src/main/java/ugovor/Start.java
+++ Ofir/Ugovor/src/main/java/ugovor/Start.java
@@ -4,6 +4,10 @@
 
 package ugovor;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import ugovor.controller.ObradaKorisnik;
 import ugovor.model.Korisnik;
 import ugovor.util.OfirException;
@@ -26,7 +30,32 @@ public class Start {
         //new Prozor().setVisible(true);
         new test().setVisible(true);
                 
-        
+        String fileName = "C:\\Users\\Lorena\\Downloads\\ugovor.txt";
+        String oldWord = "<bozepomozi>";
+        String newWord = "proba";
+
+        try {
+            FileReader reader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String line;
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while ((line = bufferedReader.readLine()) != null) {
+                stringBuilder.append(line);
+                stringBuilder.append(System.lineSeparator());
+            }
+            reader.close();
+
+            String fileContent = stringBuilder.toString();
+            String newFileContent = fileContent.replaceAll(oldWord, newWord);
+
+            FileWriter writer2 = new FileWriter("C:\\Users\\Lorena\\Downloads\\"+newWord+".txt");
+            writer2.write(newFileContent);
+            writer2.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while reading or writing to the file.");
+            e.printStackTrace();
+        }
         
         
     }
